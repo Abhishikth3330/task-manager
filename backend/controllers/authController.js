14eccc0e0f48fc25565f561e797b86b52f8f1169
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-04-17 00:58:23 +0530

    done with authController in backend, to specify the role to a user as admin or member

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,7 +10,52 @@ const generateToken = (userId) => {
 // @desc    Register a new user
 // @route   POST /api/auth/register
 // @access  Public
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, profileImageUrl, adminInviteToken } =
+            req.body;
+        
+            // check if user already exists
+            const userExists = await User.findOne({ email });
+            if (userExists) {
+                return res.status(400).json({ message: "User already exists" });
+            }
+
+            // Determine user role: Admin if correct token is provided, otherwise member
+            let role = "member";
+            if (
+                adminInviteToken &&
+                adminInviteToken == process.env.ADMIN_INVITE_TOKEN
+            ){
+                role = "admin";
+            }
+
+            // Hash Password
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            // create new user
+            const user = await User.create({
+                name,
+                email,
+                password: hashedPassword,
+                profileImageUrl,
+                role,
+            });
+
+            // Return user data with JWT
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                profileImageUrl: user.profileImageUrl,
+                token: generateToken(user._id),
+            });
+    }catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+};
 
 // @desc    Login User
 // @route   POST /api/auth/login
