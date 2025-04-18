4dd4910e37fcc06d79dfae789c8527a5831f1fb9
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-04-19 01:31:31 +0530

    done with authController.js now working on userAuthController to get users, get user id and delete user

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const connectDB = require("./config/db");
 
 
 const authRoutes = require("./routes/authRoutes")
+const userRoutes = require("./routes/userRoutes")
 
 
 
@@ -35,7 +36,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
 // app.use("/api/tasks", taskRoutes);
 // app.use("/api/reports", reportRoutes);
 
