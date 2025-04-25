9a555753b36aba46601b3b92f97b017afc560975
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-04-26 00:01:57 +0530

    started integrating APIs & done with simultaneous npm run dev of frontend and backend

--- frontend/Task-Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task-Manager/src/pages/Auth/SignUp.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout';
 import { validateEmail } from '../../utils/helper';
 import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import Input from '../../components/Inputs/Input';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -50,8 +52,53 @@ const SignUp = () => {
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            {/* Code THIS SECTION NEXT... */}
+            <Input
+              value = {fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John"
+              type="text"
+            />
+
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+    
+            <Input
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Password"
+              placeholder="Min 8 Characters"
+              type="password"
+            />
+
+            <Input
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Admin Invite Token"
+              placeholder="6 Digit Code"
+              type="text"
+            />
           </div>
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type='submit' className='btn-primary'>
+            SIGN UP
+          </button>
+
+
+          <p className='text-[13px] text-slate-800 mt-3'>
+            Already have an account? {" "}
+            <Link className="font-medium text-primary underline" to="/login">
+              Login
+            </Link>
+          </p>
+          
         </form>
       </div>
     </AuthLayout>
