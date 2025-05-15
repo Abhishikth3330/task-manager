097f3c5994f32a61e850bf545a2c9c948111b17a
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-05-15 14:10:54 +0530

    you can add tasks, attachments and added a button to create task

--- frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
+++ frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
@@ -10,6 +10,7 @@ import { LuTrash2 } from 'react-icons/lu';
 import SelectDropdown from '../../components/Inputs/SelectDropdown';
 import SelectUsers from '../../components/Inputs/SelectUsers';
 import TodoListInput from '../../components/Inputs/TodoListInput';
+import AddAttachmentsInput from '../../components/Inputs/AddAttachmentsInput';
 
 
 const CreateTask = () => {
@@ -175,6 +176,32 @@ const CreateTask = () => {
               />
             </div>
 
+            <div className="mt-3">
+                <label className='text-xs font-medium text-slate-600'>
+                  Add Attachments
+                </label>
+
+                <AddAttachmentsInput
+                  attachments={taskData?.attachments}
+                  setAttachments={(value) =>
+                    handleValueChange("attachments", value)
+                  }
+                />
+            </div>
+
+            {error && (
+              <p className="text-xs font-medium text-red-500 mt-5">{error}</p>
+            )}
+
+            <div className="flex justify-end mt-7">
+              <button
+                className="add-btn"
+                onClick={handleSubmit}
+                disabled={loading}
+              >
+                {taskId ? "UPDATE TASK" : "CREATE TASK"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
